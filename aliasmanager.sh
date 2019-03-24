dbbf428b89fc6ffbe744198ebef48f4b6fb6268c
Pedro Paiva <pedrovsp109@gmail.com>
2019-03-24 15:05:40 -0300

    Update aliasmanager file

--- aliasmanager.sh
+++ aliasmanager.sh
@@ -4,10 +4,20 @@
 alias gas='git add .'
 alias gic='git checkout'
 alias gil='git log --pretty=format:"%C(auto)%h - %an, %ar %n %s %n"'
-alias gim='git commit -a -m'
 alias gir='git reset'
 alias gis='git status -sb'
 
 # Helpful Bash Aliases for Rails
 alias rs='rails s'
 alias rc='rails c'
+alias rdb='rails db:drop db:create db:migrate'
+
+# Helpful Functions for Rails
+function econtroller() {
+  rails g controller $@ --skip-assets --no-helper
+}
+
+# Helpful Functions
+function mkcdir () {
+  mkdir -p -- "$1" && cd -P -- "$1"
+}
