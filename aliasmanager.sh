db54859c7e0697514a5e3d327bbef86bc253784b
Pedro Paiva <pedrovsp109@gmail.com>
2019-10-21 22:42:44 -0300

    Add comments

--- aliasmanager.sh
+++ aliasmanager.sh
@@ -30,18 +30,23 @@ function econtroller() {
 }
 
 # Helpful Functions
+
+# Create folder and change directory
 function mkcdir () {
   mkdir -p -- "$1" && cd -P -- "$1"
 }
 
+# Change directory and open VSCode
 function codir () {
   cd -P -- "$1" && code .
 }
 
+# Enter bash of a docker compose service
 function dcbash () {
   docker-compose exec "$1" bash
 }
 
-# function railsnew() {
-#   rails new "$1" --webpack="$2" --skip-turbolinks --skip-spring -T -d postgresql
-# }
+# Create a rails project without turbolinks, spring, minitest and using postgresql
+function newrails() {
+  rails new "$1" --skip-turbolinks --skip-spring -T -d postgresql
+}
