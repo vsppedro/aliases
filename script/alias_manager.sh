29866b74483225689dff7f92be609c57f557af56
Pedro Paiva <pedro.v.s.paiva@gmail.com>
2020-12-15 08:52:20 -0300

    Add git_rebase_master shell script

--- script/alias_manager.sh
+++ script/alias_manager.sh
@@ -2,3 +2,4 @@
 
 # Add alias for bash scripts 
 alias git_update_fork='~/aliases/script/git_update_fork.sh'
+alias git_rebase_master='~/aliases/script/git_rebase_master.sh'
---
+++ script/git_rebase_master.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run this shell script to rebase the branch with the master
+
+git pull origin master
+
+git rebase master
